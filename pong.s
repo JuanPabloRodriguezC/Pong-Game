06747cbf1c9b93c126393bb5b4f1e0fbb74d1518
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2025-04-07 18:00:36 -0600

    mueve pala izquierda

--- pong.s
+++ pong.s
@@ -3,19 +3,75 @@ li a1 LED_MATRIX_0_WIDTH
 li a2 LED_MATRIX_0_HEIGHT
 li a3 D_PAD_0_BASE
 li a4 0xffffff
-li a5 0x000005
+li a5 5
+li a6 0
+li s0 1
+li s1 0
 
-colorea_paleta:
-    add t0, zero, a1
-    li t1, 4
-    mul t2, t0, t1
+loop_juego:
+    jal dibuja_pala
+
+actualiza_pala:
+    # verifica si boton de arriba se presiona
+    lw t1, 0(a3)
+    andi t1, t1, 1
+    beqz t1, check_down_button
     
-    add t3, a0, t2
-    sw a4, 0(t3)
-    addi t4, t4, 1
+    # boton de arriba presionado
+    addi t2, s0, 0
+    beqz t2, boton_abajo    # verifica limite superior
+
+    add t0, zero, a0    # direccion base
+    li t2, 4            # byte
+    mul t3, a1, t2      # bytes por fila
     
-colorea_paleta_loop:
-    add t3, t3, t2
-    sw a4, 0(t3)
-    addi t4, t4, 1
-    bne t4, a5, colorea_paleta_loop
+    addi t1, s0, 4
+    mul t4, t1, t3      # offset actual
+    add t0, t0, t4      # agrega offset
+
+    sw a6, 0(t0)        # color
+
+    addi s0, s0, -1
+
+boton_abajo:
+    # verifica boton de abajo presionado
+    lw t1, 4(a3)
+    andi t1, t1, 1
+    beqz t1, dibuja_pala
+    
+    # boton de abajo presionado
+    addi t2, s0, 0
+    sub t3, a2, a5
+    bge t2, t3, dibuja_pala   # verifica limite inferior
+
+    add t0, zero, a0    # direccion base
+    li t2, 4            # byte
+    mul t3, a1, t2      # bytes por fila
+    
+    mul t4, s0, t3      # offset actual
+    add t0, t0, t4      # agrega offset
+
+    sw a6, 0(t0)        # color
+
+    addi s0, s0, 1
+
+    
+    
+dibuja_pala:
+    add t0, zero, a0    # direccion base
+    li t1, 1            # contador
+    li t2, 4            # byte
+    mul t3, a1, t2      # bytes por fila
+    
+    mul t4, s0, t3      # offset actual
+    add t0, t0, t4      # agrega offset
+
+    sw a4, 0(t0)        # color
+    
+dibuja_pala_loop:
+    add t0, t0, t3      # direccion pixel siguiente fila 
+    addi t1, t1, 1      # incrementa counter
+    sw a4, 0(t0)        # color 
+    bne t1, a5, dibuja_pala_loop
+    beq t1, a5, actualiza_pala
+
